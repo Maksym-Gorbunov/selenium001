585ae0126ed9b68c6b1b40ad23d35ba4d39ce97a
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-06-27 19:12:32 +0200

    properties

---
+++ src/config/PropertiesFile.java
@@ -0,0 +1,65 @@
+package config;
+
+import test001.SeleniumTest001;
+
+import java.io.*;
+import java.util.Properties;
+
+public class PropertiesFile {
+  static Properties prop = new Properties();
+  static String projectPath = System.getProperty("user.dir");
+
+  public static void main(String[] args) {
+    writePropertiesFile();
+    readPropertiesFile();
+  }
+
+  //Get data from config file
+  public static void readPropertiesFile(){
+    try{
+      InputStream input = new FileInputStream(projectPath+"\\src\\config\\config.properties");
+      prop.load(input);
+      System.out.println(prop.getProperty("browser"));
+      SeleniumTest001.browser = prop.getProperty("browser");
+    }catch (Exception e){
+      e.printStackTrace();
+    }
+  }
+
+  //Write properties to config
+  public static void writePropertiesFile(){
+    try {
+      OutputStream output = new FileOutputStream(projectPath+"\\src\\config\\config.properties");
+      prop.setProperty("browser", "Chrome");
+      prop.setProperty("test", "ok");
+      //prop.store(output, "Current webbrowser");
+      prop.store(output, null);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  // Add/edit property in config file
+  public static void writeProperty(String property, String value){
+    try {
+      OutputStream output = new FileOutputStream(projectPath+"\\src\\config\\config.properties");
+      prop.setProperty(property, value);
+      prop.store(output, null);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  // Get property value from config file
+  public static String getPropertyValue(String property){
+    try{
+      InputStream input = new FileInputStream(projectPath+"\\src\\config\\config.properties");
+      prop.load(input);
+      return prop.getProperty(property);
+    }catch (Exception e){
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+}
--- src/test001/SeleniumTest001.java
+++ src/test001/SeleniumTest001.java
@@ -1,21 +1,27 @@
 package test001;
 
+import config.PropertiesFile;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class Test001 {
-  static String browser;
+public class SeleniumTest001 {
+  public static String browser;
   static WebDriver driver;
+
   public static void main(String[] args) {
-    setBrowser();
+    //setBrowser("Chrome");
+    PropertiesFile.readPropertiesFile();
     setBrowserConfig();
     runTest();
+    PropertiesFile.writeProperty("browser", "Chrome");
+    System.out.println(PropertiesFile.getPropertyValue("empty"));
+    System.out.println(PropertiesFile.getPropertyValue("browser"));
   }
 
   //set Browser
-  public static void setBrowser(){
-    browser = "Firefox";
+  public static void setBrowser(String browserName){
+    browser = browserName;
   }
 
   //set Browser Config
@@ -34,6 +40,6 @@ public class Test001 {
   //run Test
   public static void runTest(){
     driver.get("https://www.seleniumhq.org/");
-    driver.quit();
+    //driver.quit();
   }
 }
