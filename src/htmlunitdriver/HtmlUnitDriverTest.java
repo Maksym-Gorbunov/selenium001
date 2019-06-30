3bad1113b0909d13dc82fcc5c812059cf0c74b6d
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-06-30 23:30:45 +0200

    browser version

---
+++ src/htmlunitdriver/HtmlUnitDriverTest.java
@@ -0,0 +1,29 @@
+package htmlunitdriver;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import java.lang.reflect.Field;
+
+public class HtmlUnitDriverTest {
+  public static void main(String[] args) throws Exception {
+    //HtmlUnitDriver driver = new HtmlUnitDriver();
+    HtmlUnitDriver driver = new HtmlUnitDriver(BrowserVersion.CHROME);
+    driver.get("https://www.seleniumhq.org/");
+    System.out.println("Title: " + driver.getTitle());
+
+
+    //WebClient
+    WebClient webClient = (WebClient) get(driver, "WebClient");
+
+    driver.quit();
+  }
+
+  //Get browser version
+  private static Object get(Object object, String field) throws Exception{
+    Field f = object.getClass().getDeclaredField(field);
+    f.setAccessible(true);
+    return f.get(object);
+  }
+}
