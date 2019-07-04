98b6a947b718202b99b9eaec8df673e45512e058
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-07-04 14:24:03 +0200

    phantom not supported

---
+++ src/Temp.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Temp {
+  public static void main(String[] args) {
+    String[] s = new String[]{"hello","toskjd"};
+    Arrays.stream(s).forEach(line-> System.out.println(line));
+    Arrays.stream(s).forEach(line-> System.out.println(line));
+
+  }
+}
